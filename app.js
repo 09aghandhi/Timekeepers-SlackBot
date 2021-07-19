6a92690fb5f74b8af0cd1de3aece0441e92681b4
Atul Ghandhi <atul.ghandhi@and.digital>
2021-07-19 12:32:34 +0100

    Setting up slackbot

---
+++ app.js
@@ -0,0 +1,5 @@
+
+
+console.log("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"Hello, World!\"}' https://hooks.slack.com/services/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");
+
+
